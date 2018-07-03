09e276d94862845c8a6ecabd296d82840b643cb5
SirTaphos <BjornFrierPedersen@gmail.com>
2018-07-03 13:52:31 +0200

    Add files via upload
    My implementation of the popular FooBar code-challenge

---
+++ Opgave1_FooBar/Opgave1_FooBar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Opgave1_FooBar
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0) Console.WriteLine("FooBar");
+                else if(i % 3 == 0) Console.WriteLine("Foo");
+                else if (i % 5 == 0) Console.WriteLine("Bar");
+                else Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
